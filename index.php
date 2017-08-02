815f206cb9aa49eda75450898f368f1e17b43279
Mark (Laptop) <mark@pixelsmiths.co.uk>
2017-08-02 21:09:15 +0100

    Initial Committ

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<link rel="stylesheet" href="css/universal.css">
+	<title></title>
+	<meta name="description" content="">
+</head>
+<body>
+	<div class="page-wrapper">
+		<header>
+			<nav class="main-nav">
+
+			</nav>
+		</header>
+			<main class="homepage-main-content">
+				<h1>Hello World</h1>
+			</main>
+	</div> <!-- Wrapper div -->
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="js/jquery/jquery2.1.4"><\/script>')</script>
+<script type="text/javascript" src="scripts/universal-min.js"></script>
+</body>
+
+</html>
