5bf1f63be67f8681a9e160a580890cc1014023c7
Mark (Laptop) <mark@pixelsmiths.co.uk>
2017-08-05 12:50:50 +0100

    Scrolling images

--- index.php
+++ index.php
@@ -16,28 +16,44 @@
 		</header>
 			<main class="homepage-main-content">
 				<h1>Hello World</h1>
-				<section class="section-one image-section"></section>
+				<section class="section-one image-section">
+					<div class="image-div div-one rellax" data-rellax-speed="3" data-rellax-percentage="0.1">
+
+					</div>
+				</section>
 				<section class="section-two text-section">
 					<h2>Heading</h2>
 					<p>
 						Lorem ipsum dolor sit amet, consectetur adipisicing elit. At excepturi saepe numquam modi, consectetur, laborum aut magni quis qui quod quam, quos fugiat! Sed voluptatibus eaque nam cumque inventore quod, quis voluptates autem in, reprehenderit! Esse rem minima, cum, ipsum repudiandae molestias laboriosam laudantium recusandae alias voluptate provident, unde quaerat? Amet ipsa eum est quis quasi odit facere ab, dolore dolorem soluta in delectus doloremque accusamus, iure ex eos porro recusandae quas corrupti, ipsum libero tenetur? Dolorum praesentium deleniti odio minima similique quisquam voluptatibus quaerat optio ad molestias, culpa nisi ea autem repellendus quia assumenda eaque mollitia veritatis. Neque harum fuga quasi ullam blanditiis optio sequi expedita dolores tempora rerum a rem illum eaque, placeat reprehenderit ipsa ipsam deserunt non, debitis vel assumenda nisi quis voluptas enim. Fuga ducimus quas eveniet voluptatum, suscipit eaque nostrum inventore praesentium voluptas! Ipsam vitae placeat voluptates adipisci, corporis maiores! Reprehenderit culpa velit dolorem quod.
 					</p>
 				</section>
-				<section class="section-three image-section"></section>
+				<section class="section-three image-section">
+					<div class="image-div div-two rellax" data-rellax-speed="3" data-rellax-percentage="0.1">
+
+					</div>
+				</section>
 				<section class="section-four text-section">
 					<h2>Heading</h2>
 					<p>
 						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente, qui eius, velit voluptatibus dolores minus earum facere quaerat modi, reiciendis vero. Placeat corporis aut fugiat impedit odit minus necessitatibus exercitationem sunt, expedita cupiditate nisi officia quod similique culpa velit nam laborum voluptates, eius dolores. Maxime laudantium optio voluptatibus quasi ipsa dolore quae. Labore pariatur blanditiis eaque necessitatibus, aut a fugit? Voluptatem perspiciatis minus cumque nisi ipsum molestiae beatae consequuntur quasi iste perferendis officiis, quo quidem ad sed deserunt modi dolor, at vitae repellendus quam, delectus blanditiis. Eaque, deleniti provident quae architecto quam voluptatem incidunt accusamus ratione fuga commodi voluptatibus ipsa expedita suscipit cum, eveniet odit modi repellat quia libero saepe laborum! Iure non vel consequuntur sunt, dignissimos officia rerum praesentium! Blanditiis dignissimos libero maxime corrupti, error rem enim consectetur quibusdam, aut ex alias quo veritatis voluptatum dolor odio amet vero, ab et unde vel assumenda. Aut dignissimos debitis, incidunt officia!
 					</p>
 				</section>
-				<section class="section-five image-section"></section>
+				<section class="section-five image-section">
+					<div class="image-div div-three rellax" data-rellax-speed="3" data-rellax-percentage="0.1">
+
+					</div>
+				</section>
 				<section class="section-six text-section">
 					<h2>Heading</h2>
 					<p>
 						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio illum pariatur a molestiae, quae rerum, facere eius consequuntur ratione ipsum accusamus, esse illo enim iusto labore. Iste unde earum facilis. Quibusdam sint, tenetur quae illo eveniet at vitae ipsum doloribus libero harum debitis a ad reprehenderit ducimus quisquam, aliquam, accusantium alias non natus deleniti veritatis, deserunt. Voluptas facere sapiente tenetur fugiat molestiae iste explicabo dicta sint asperiores quas deserunt porro magni debitis nemo quod, velit ea veritatis doloremque minima neque pariatur deleniti qui in laboriosam. Distinctio mollitia quo, nisi cupiditate, delectus vitae repellat similique ipsa animi maxime! Quia itaque cupiditate, fugiat similique eos est, illum veritatis accusantium, at explicabo molestias eum. Corporis alias ullam obcaecati laboriosam ex, error sit! Rerum, quos, veniam. Et labore, architecto. Sunt culpa iusto totam modi est aperiam ut et exercitationem praesentium eaque, magni nisi repellat. Harum ipsam velit cumque possimus nemo aperiam nisi optio excepturi.
 					</p>
 				</section>
-				<section class="section-seven image-section"></section>
+				<section class="section-seven image-section">
+					<div class="image-div div-four rellax" data-rellax-speed="3" data-rellax-percentage="0.1">
+
+					</div>
+				</section>
 				<section class="section-eight text-section">
 					<h2>Heading</h2>
 					<p>
