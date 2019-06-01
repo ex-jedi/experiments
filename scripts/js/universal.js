8235ab7f9c6c45bdca3279dbcedb9261437101b3
Mark Phoenix <mark@pixelsmiths.co.uk>
2019-06-01 07:51:58 +0100

    in progress

--- scripts/js/universal.js
+++ scripts/js/universal.js
@@ -1,42 +1,72 @@
-// Detect request animation frame
-let scroll =
-  window.requestAnimationFrame ||
-  // IE Fallback
-  function(callback) {
-    window.setTimeout(callback, 1000 / 60);
-  };
-
-let elementsToShow = document.querySelectorAll('.show-on-scroll');
-
-function loop() {
-  elementsToShow.forEach(function(element) {
-    if (isElementInViewport(element)) {
-      element.classList.add('red-or-ded');
-    } else {
-      element.classList.remove('red-or-ded');
-    }
-  });
-
-  scroll(loop);
-}
-
-// Call the loop for the first time
-loop();
-
-// Helper function from: http://stackoverflow.com/a/7557433/274826
-function isElementInViewport(el) {
-  var bounding = el.getBoundingClientRect();
-
-  //Set how far the element 'peeks' into the viewport
-  let offsetTop = 50; // Offset in pixels or bounding.height * 0.9 (fraction of elements height)
-  let offsetBottom = 150; // Offset in pixels or bounding.height * 0.9 (fraction of elements height)
-
-  let offsetFirst = offsetTop - bounding.height;
-  let offsetSecond = bounding.height - offsetBottom;
-  return (
-    bounding.top >= offsetFirst &&
-    bounding.bottom <=
-      (window.innerHeight + offsetSecond ||
-        document.documentElement.clientHeight + offsetSecond)
-  );
-}
+particlesJS('particles-js', {
+  particles: {
+    number: { value: 50, density: { enable: true, value_area: 800 } },
+    color: { value: '#ffffff' },
+    shape: {
+      type: 'circle',
+      stroke: { width: 0, color: '#000000' },
+      polygon: { nb_sides: 5 },
+      image: { src: 'img/github.svg', width: 100, height: 100 },
+    },
+    opacity: {
+      value: 0.5,
+      random: false,
+      anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false },
+    },
+    size: {
+      value: 3,
+      random: true,
+      anim: { enable: false, speed: 40, size_min: 0.1, sync: false },
+    },
+    line_linked: {
+      enable: true,
+      distance: 150,
+      color: '#ffffff',
+      opacity: 0.4,
+      width: 1,
+    },
+    move: {
+      enable: true,
+      speed: 6,
+      direction: 'none',
+      random: false,
+      straight: false,
+      out_mode: 'bounce',
+      bounce: false,
+      attract: { enable: false, rotateX: 600, rotateY: 1200 },
+    },
+  },
+  interactivity: {
+    detect_on: 'canvas',
+    events: {
+      onhover: { enable: true, mode: 'grab' },
+      onclick: { enable: true, mode: 'repulse' },
+      resize: true,
+    },
+    modes: {
+      grab: { distance: 400, line_linked: { opacity: 1 } },
+      bubble: { distance: 400, size: 40, duration: 2, opacity: 8, speed: 3 },
+      repulse: { distance: 200, duration: 0.4 },
+      push: { particles_nb: 4 },
+      remove: { particles_nb: 2 },
+    },
+  },
+  retina_detect: true,
+});
+var count_particles, stats, update;
+stats = new Stats();
+stats.setMode(0);
+stats.domElement.style.position = 'absolute';
+stats.domElement.style.left = '0px';
+stats.domElement.style.top = '0px';
+document.body.appendChild(stats.domElement);
+count_particles = document.querySelector('.js-count-particles');
+update = function() {
+  stats.begin();
+  stats.end();
+  if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
+    count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
+  }
+  requestAnimationFrame(update);
+};
+requestAnimationFrame(update);
