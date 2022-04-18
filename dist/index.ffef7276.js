3cb2346c57aebf5ad2acba032cbc4b5c9b4064ef
Mark Phoenix <mark@pixelsmiths.co.uk>
2022-04-18 19:54:10 +0100

    Parcel and tidy up

---
+++ dist/index.ffef7276.js
@@ -0,0 +1,14 @@
+const removeIt = document.querySelectorAll('div div');
+// const wait = ms => new Promise(resolve => setTimeout(resolve, ms));
+async function beGone(e) {
+    const { currentTarget  } = e;
+    currentTarget.classList.add('animate');
+    currentTarget.addEventListener('transitionend', ()=>{
+        console.log(currentTarget);
+        currentTarget.remove();
+    });
+}
+removeIt.forEach((el)=>el.addEventListener('click', beGone)
+);
+
+//# sourceMappingURL=index.ffef7276.js.map
--- scripts/minified/universal-min.js
+++
@@ -1,2 +0,0 @@
-const removeIt=document.querySelectorAll("div div");async function beGone(e){const{currentTarget:n}=e;n.classList.add("animate"),n.addEventListener("transitionend",()=>{console.log(n),n.remove()})}removeIt.forEach(e=>e.addEventListener("click",beGone));
-//# sourceMappingURL=universal-min.js.map
